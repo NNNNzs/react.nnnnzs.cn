11963ea8d3ef3e42e32e03249230a279dd3c521d
NNNNzs <nnnnzs@vip.qq.com>
2026-07-28 17:43:11 +0800

    feat: 添加google广告

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,8 @@ export default async function RootLayout({
                   })();
                   `}
                     </Script>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6540786548340758" crossorigin="anonymous"></script>
+     
                     <GoogleTagManager gtmId="GTM-PTJQT23X" />
                     <GoogleAnalytics measurementId={analyticsConfig.measurementId} />
                   </TaskNotificationProvider>
